42e55a8d3a931b901bf4f1949a5bc6b3ffd39927
Spartak <79632617+ForsakenWing@users.noreply.github.com>
2024-08-25 00:23:46 +0400

    Big update! Devops time is coming :) (#17)
    * Big update! Devops time is coming :)
    
    * Updating  unit-tests and integration tests
    
    * Updating package + testing image build locally

--- tests/integration_tests/test_integration_main.py
+++ tests/integration_tests/test_integration_main.py
@@ -12,46 +12,49 @@ headers = {"Accept": "application/json", "Host": host}
 
 
 def test_get_echo_reply():
-    response = client.get(base_url + '/', headers=headers)
+    response = client.get(base_url + "/", headers=headers)
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, stranger"}
+    assert "text/html" in response.headers["content-type"]
+    assert "<title>Some HTML in here</title>" in response.text
+    assert "<h1>Look ma! HTML!</h1>" in response.text
 
 
-@pytest.mark.parametrize("test_input", [
-    ({"name": "Vasiliy"}),
-    ({"surname": "Petrov"}),
-    ({})
-])
+@pytest.mark.parametrize(
+    "test_input", [({"name": "Vasiliy"}), ({"surname": "Petrov"}), ({})]
+)
 def test_post_echo_reply(test_input):
-    response = client.post(base_url + '/', headers=headers, json=test_input)
+    response = client.post(base_url + "/", headers=headers, json=test_input)
     assert response.status_code == 200
     assert json.loads(response.json()) == test_input
 
 
 def test_get_request_parser():
     session = client.Session()
-    response = session.get(base_url + '/request', headers=headers)
+    response = session.get(base_url + "/request", headers=headers)
     assert response.status_code == 200
     assert response.json() == {
         "headers": {k.lower(): v for k, v in dict(response.request.headers).items()},
         "cookies": dict(session.cookies),
         "method": response.request.method,
-        "body": {}
+        "body": {},
     }
 
 
-@pytest.mark.parametrize("test_input", [
-    ({"name": "Vasya", "surname": "Vasiliev"}),
-    ({"color": "black", "something": 5}),
-    ({})
-])
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        ({"name": "Vasya", "surname": "Vasiliev"}),
+        ({"color": "black", "something": 5}),
+        ({}),
+    ],
+)
 def test_post_request_parser(test_input):
     session = client.Session()
-    response = client.post(base_url + '/request', headers=headers, json=test_input)
+    response = client.post(base_url + "/request", headers=headers, json=test_input)
     assert response.status_code == 200
     assert response.json() == {
         "headers": {k.lower(): v for k, v in dict(response.request.headers).items()},
         "cookies": dict(session.cookies),
         "method": response.request.method,
-        "body": test_input
+        "body": test_input,
     }
